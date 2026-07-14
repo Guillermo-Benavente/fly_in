b89956728dc0fee19a57740a18dd51bcc935a608
Guillermo Benavente Mora <gbenaven@c1r7s5.42madrid.com>
2026-07-14 17:42:07 +0200

    intento de ruta

--- route_planner.py
+++ route_planner.py
@@ -8,6 +8,7 @@ class RoutePlanner():
     
     def __init__(self, network_zone: NetworkZone) -> None:
         self.network_zone = network_zone
+        self.routes()
 
     def routes(self):
         drones: list[Drone] = self.drones()
@@ -27,13 +28,22 @@ class RoutePlanner():
         pass
 
     def map(self) -> dict[str, int]:
-        map: dict[str, int] = {'goal': 0}
+        map: dict[str, dict[str, int]] = {}
         queue: list[Hub] = [self.network_zone.end]
         while queue:
             current_node = queue.pop(0)
             for connection in self.network_zone.find_connection(current_node):
                 if connection.final_hub == current_node:
                     init_weight: int = connection.init_hub.get_turn_zone()
-                    new_weight: int = map[current_node.name] + init_weight
-                    map[connection.init_hub.name] = new_weight
+                    new_weight: int = map[current_node.name] or 0
+                    if init_weight == -1:
+                        new_weight = -1
+                    else:
+                        new_weight += init_weight
+                    map[connection.init_hub.name][current_node.name] = new_weight
+                    queue.append(connection.init_hub)
         return map
+{start:{waypoint1:3}}
+{waypoint1:{start:-2,waypoint2:2}}
+{waypoint2:{waypoint1:-3,goal:1}}
+{goal:{waypoint2:-4, goal:-1}}
